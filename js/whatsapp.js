2c9da17d8e729ee1f466c84281f284c7894b9747
JosephGato <joseph.g.muriuki@gmail.com>
2025-01-23 11:13:48 +0300

    whatsapp

---
+++ js/whatsapp.js
@@ -0,0 +1,6 @@
+// Track clicks on the WhatsApp button
+const whatsappButton = document.querySelector('.whatsapp-button');
+whatsappButton.addEventListener('click', () => {
+  console.log('WhatsApp button clicked!');
+  // You can add custom tracking or analytics code here
+});
